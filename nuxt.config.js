f0d403887e0cfa85240c868a5b867715ae46985b
vadimborovik <v.borovuk@softum.io>
2025-03-14 09:39:05 +0200

    set color theme - light

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,23 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/ui',
   ],
-
   css: ['~/assets/css/main.css'],
-
   future: {
     compatibilityVersion: 4
   },
-
   compatibilityDate: '2024-11-27',
   colorMode: {
-    preference: 'dark', // default value of $colorMode.preference
-    fallback: 'dark', // fallback value if not system preference found
-    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
-    storageKey: 'nuxt-color-mode'
+    preference: 'light', // За замовчуванням буде 'light'
+    dataValue: 'theme',  // Додає `data-theme="light"` у <html>
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode' // Ключ для LocalStorage
   },
   tailwindcss: {
     exposeConfig: true // Додає можливість отримувати налаштування Tailwind в коді
@@ -25,7 +21,6 @@ export default defineNuxtConfig({
   dayjs: {
     plugins: ['utc', 'timezone'], // Підключаємо потрібні плагіни
   },
-
   typescript: {
     strict: false,
     shim: false
